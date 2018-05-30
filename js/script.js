c25c2855959c7304c1c4ff3c8e51983324b8e43d
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-05-30 06:01:49 +0100

    Leaflet loading

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initMap() {
+	// set up the map
+	map = new L.Map('map');
+
+	// create the tile layer with correct attribution
+	var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(57.3619, -6.24727),9);
+	map.addLayer(osm);
+}
+
+initMap();

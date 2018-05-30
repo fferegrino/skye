30435becfe5dfad18df01673d406b5696b961001
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-05-30 06:36:14 +0100

    leaflet-sidebar-v2 integration

--- js/script.js
+++ js/script.js
@@ -2,19 +2,44 @@ var map;
 var ajaxRequest;
 var plotlist;
 var plotlayers=[];
+var sidebar;
 
 function initMap() {
-	// set up the map
-	map = new L.Map('map');
+    var map = L.map('map');
+    map.setView([57.3619, -6.24727], 9);
 
-	// create the tile layer with correct attribution
-	var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; OpenStreetMap contributors'
+    }).addTo(map);
 
-	// start the map in South-East England
-	map.setView(new L.LatLng(57.3619, -6.24727),9);
-	map.addLayer(osm);
+    var marker = L.marker([51.2, 7]).addTo(map);
+
+    sidebar = L.control.sidebar({
+        autopan: false,       // whether to maintain the centered map point when opening the sidebar
+        closeButton: true,    // whether t add a close button to the panes
+        //container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+        position: 'left',     // left or right
+    }).addTo(map);
+
+        /* add a new panel */
+    var panelContent = {
+        id: 'userinfo',                     // UID, used to access the panel
+        tab: '<i class="fa fa-info"></i>',  // content can be passed as HTML string,
+        //pane: someDomNode.innerHTML,        // DOM elements can be passed, too
+        title: 'Your Profile',              // an optional pane header
+        position: 'bottom'                  // optional vertical alignment, defaults to 'top'
+    };
+    sidebar.addPanel(panelContent);
+
+    /* add an button with click listener */
+    sidebar.addPanel({
+        id: 'click',
+        tab: '<i class="fas fa-thumbs-up"></i>',
+        button: function (event) { console.log(event); }
+    });
+
+    
 }
 
 initMap();

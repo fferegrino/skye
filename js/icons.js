ec0e6c1a7f1fa2c2a4e26183ef9956789abb3d71
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-06-01 20:20:32 +0100

    New pins

--- js/icons.js
+++ js/icons.js
@@ -1,11 +1,16 @@
 var meetIcon = L.icon({
     iconUrl: 'img/pins/meet.png',
-//    shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [36, 48], // size of the icon
-//    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [18, 40], // point of the icon which will correspond to marker's location
-//    shadowAnchor: [4, 62],  // the same for the shadow
-//    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize:     [22, 32], // size of the icon
+    iconAnchor:   [11, 32], // point of the icon which will correspond to marker's location
 });
 
+var foodIcon = L.icon({
+    iconUrl: 'img/pins/food.png',
+    iconSize:     [22, 32], // size of the icon
+    iconAnchor:   [11, 32], // point of the icon which will correspond to marker's location
+});
+
+var icon_dict = {
+    "users":meetIcon,
+    "utensils":foodIcon
+}

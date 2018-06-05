6a3b3652fd3f918e3c2af23c65a9cad32bc3e65b
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-06-05 08:37:40 +0100

    descriptions using markdown

--- js/handlebars.js
+++ js/handlebars.js
@@ -2,3 +2,8 @@ Handlebars.registerHelper('formatTime', function (date, format) {
     var mmnt = moment(date);
     return mmnt.format(format);
 });
+
+var md = window.markdownit();
+Handlebars.registerHelper('markdown', function (markdown){
+    return md.render(markdown);
+})
